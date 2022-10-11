040dd6a741f3f1176d67f3b0f11e9e4962d784fd
hellodan3000 <s2084941@ed.ac.uk>
2022-10-11 11:15:11 +0100

    Finished tutorial script - challenge still to do

--- data_manip.R
+++ data_manip.R
@@ -3,7 +3,7 @@
 # 10/10/2022
  
 
-# Subset, extract and modify data with R operators ----
+# 1. Subset, extract and modify data with R operators ----
 
 # Load the elongation data
 elongation <- read.csv("EmpetrumElongation.csv", header = TRUE)   
@@ -29,7 +29,7 @@ elongation[6, ]$Indiv   # returns the value in the column Indiv for the sixth ob
 
 # But with [ , ] if you add rows later on it might be referring to the wrong one!
 
-# So use logical operations ----
+# Use logical operations ----
 
 # Let's access the values for Individual number 603
 elongation[elongation$Indiv == 603, ]
@@ -95,7 +95,7 @@ rep(seq(0, 30, 10), 4)   # give?
 # Creating and overwriting objects ----
 
 
-## CHANGING VARIABLE NAMES AND VALUES IN A DATA FRAME ----
+# CHANGING VARIABLE NAMES AND VALUES IN A DATA FRAME ----
 
 # Let's create a working copy of our object
 elong2 <- elongation
@@ -119,7 +119,7 @@ elong2[elong2$ID == 373, ]$X2008 <- 5.7   # completely equivalent to option 1
 
 # Option 2 code will run even if the observation moves in your dataset
 
-## CREATING A FACTOR ----
+# CREATING A FACTOR ----
 
 # Let's check the classes
 str(elong2)
@@ -129,7 +129,7 @@ str(elong2)
 elong2$zone <- as.factor(elong2$zone)        # converting and overwriting original class
 str(elong2)                                  # now zone is a factor with 6 levels
 
-## CHANGING A FACTOR'S LEVELS ----
+# CHANGING A FACTOR'S LEVELS ----
 
 levels(elong2$zone)  # shows the different factor levels
 
@@ -141,3 +141,171 @@ levels(elong2$zone) <- c("A", "B", "C", "D", "E", "F")   # you can overwrite the
 
 
 
+
+
+# 2. What is tidy data? ----
+
+# Using tidyr to create long-format dataframes ----
+
+install.packages("tidyr")  # install the package
+library(tidyr)             # load the package
+
+
+elongation_long <- gather(elongation, Year, Length,                           # in this order: data frame, key, value
+                          c(X2007, X2008, X2009, X2010, X2011, X2012))        # we need to specify which columns to gather
+
+# Here we want the lengths (value) to be gathered by year (key)
+
+# Let's reverse! spread() is the inverse function, allowing you to go from long to wide format
+elongation_wide <- spread(elongation_long, Year, Length)
+
+
+
+
+# Notice how we used the column names to tell gather() which columns to reshape
+
+#  However, if you have a dataset with columns for 100 genes, for instance, 
+# you might be better off specifying the column numbers:
+
+
+elongation_long2 <- gather(elongation, Year, Length, c(3:8))
+
+
+# However, these functions have limitations and will not work on every data structure!
+
+
+# With the data in the right format, it’s much easier to analyse them and visualise the results
+
+
+# E.g. if we want to find out if there is inter-annual variation in the growth of 
+# Empetrum hermaphroditum, we can quickly make a boxplot:
+
+
+
+boxplot(Length ~ Year, data = elongation_long,
+        xlab = "Year", ylab = "Elongation (cm)",
+        main = "Annual growth of Empetrum hermaphroditum")
+
+
+
+
+# 3. Functions of dplyr ----
+
+
+install.packages("dplyr")  # install the package
+library(dplyr)              # load the package
+
+# rename() ----
+
+# lets you change the name(s) of a column or columns
+
+# he first argument is the data frame, the second (and third, etc.) takes the form New name = Old name
+
+
+elongation_long <- rename(elongation_long, zone = Zone, indiv = Indiv, year = Year, length = Length)     # changes the names of the columns (getting rid of capital letters) and overwriting our data frame
+
+# As we saw earlier, the base R equivalent would have been
+names(elongation_long) <- c("zone", "indiv", "year", "length")
+
+
+
+
+# filter() rows and select() columns ----
+
+# FILTER OBSERVATIONS BY ROW
+
+# Let's keep observations from zones 2 and 3 only, and from years 2009 to 2011
+
+elong_subset <- filter(elongation_long, zone %in% c(2, 3), year %in% c("X2009", "X2010", "X2011")) # you can use multiple different conditions separated by commas
+
+# For comparison, the base R equivalent would be (not assigned to an object here):
+# elongation_long[elongation_long$zone %in% c(2,3) & elongation_long$year %in% c("X2009", "X2010", "X2011"), ]
+
+# Note that here, we use %in% as a logical operator because we are looking to match a list of exact (character) values. 
+# If you want to keep observations within a range of numeric values, you either need two logical statements in your filter() function, 
+# e.g. length > 4 & length <= 6.5 or you can use the convenient between() function, e.g. between(length, 4, 6.5).
+
+
+# SELECT COLUMNS
+
+# Let's ditch the zone column just as an example
+
+elong_no.zone <- dplyr::select(elongation_long, indiv, year, length)   # or alternatively
+elong_no.zone <- dplyr::select(elongation_long, -zone) # the minus sign removes the column
+
+# For comparison, the base R equivalent would be (not assigned to an object here):
+elongation_long[ , -1]  # removes first column
+
+# A nice hack! select() lets you rename and reorder columns on the fly
+elong_no.zone <- dplyr::select(elongation_long, Year = year, Shrub.ID = indiv, Growth = length)
+
+# Neat, uh?
+
+
+
+# mutate() your dataset by creating new columns ----
+
+# CREATE A NEW COLUMN
+
+elong_total <- mutate(elongation, total.growth = X2007 + X2008 + X2009 + X2010 + X2011 + X2012)
+
+
+# group_by() ----
+
+
+# you don’t see any visible change to your data frame. 
+# It creates an internal grouping structure, which means that every subsequent function you run on it 
+# will use these groups, and not the whole dataset, as an input.
+
+
+# GROUP DATA
+
+elong_grouped <- group_by(elongation_long, indiv)   # grouping our dataset by individual
+
+# elong_grouped and elongation_long : they should look exactly the same. 
+# But now, let’s use summarise() to calculate total growth of each individual over the years.
+
+
+# summarise() data with summary statistics ----
+
+# SUMMARISING OUR DATA
+
+summary1 <- summarise(elongation_long, total.growth = sum(length))
+summary2 <- summarise(elong_grouped, total.growth = sum(length))
+
+
+summary3 <- summarise(elong_grouped, total.growth = sum(length),
+                      mean.growth = mean(length),
+                      sd.growth = sd(length))
+
+
+
+# ..._join() datasets based on shared attributes ----
+
+
+# Load the treatments associated with each individual
+
+treatments <- read.csv("EmpetrumTreatments.csv", header = TRUE, sep = ";")
+head(treatments)
+
+# Join the two data frames by ID code. The column names are spelled differently, 
+# so we need to tell the function which columns represent a match. 
+# We have two columns that contain the same information in both datasets: zone and individual ID.
+
+experiment <- left_join(elongation_long, treatments, by = c("indiv" = "Indiv", "zone" = "Zone"))
+
+# We see that the new object has the same length as our first data frame, 
+# which is what we want. And the treatments corresponding to each plant have been added!
+
+
+# The equivalent base R function is merge() and actually works very well, too:
+
+experiment2 <- merge(elongation_long, treatments, by.x = c("zone", "indiv"), by.y = c("Zone", "Indiv"))  
+# same result!
+
+
+# Now that we have gone to the trouble of adding treatments into our data, 
+# let’s check if they affect growth by drawing another box plot.
+
+
+boxplot(length ~ Treatment, data = experiment)
